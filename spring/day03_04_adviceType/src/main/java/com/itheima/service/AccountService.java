c40d5f286e38448ead6b4f8e3347d480b9f24e41
lyj1998 <3301056761@qq.com>
2021-11-05 22:17:53 +0800

    aop

---
+++ spring/day03_04_adviceType/src/main/java/com/itheima/service/AccountService.java
@@ -0,0 +1,19 @@
+package com.itheima.service;
+
+/**
+ * 账户的业务层接口
+ */
+public interface AccountService {
+    /**
+     * 模拟保存账户
+     */
+    void saveAccount();
+    /**
+     * 模拟更新账户
+     */
+    void updateAccount(int i);
+    /**
+     * 删除账户
+     */
+    int deleteAccount();
+}
---
+++ spring/day03_04_adviceType/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -0,0 +1,30 @@
+package com.itheima.service.impl;
+
+import com.itheima.service.AccountService;
+
+public class AccountServiceImpl implements AccountService {
+    /**
+     * 模拟保存账户
+     */
+    public void saveAccount() {
+        System.out.println("执行了保存");
+        int i = 1/0;
+    }
+
+    /**
+     * 模拟更新账户
+     * @param i
+     */
+    public void updateAccount(int i) {
+        System.out.println("执行了更新");
+    }
+
+    /**
+     * 删除账户
+     * @return
+     */
+    public int deleteAccount() {
+        System.out.println("执行了删除");
+        return 0;
+    }
+}
